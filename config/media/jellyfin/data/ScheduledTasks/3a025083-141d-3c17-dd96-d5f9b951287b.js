a40651a9c5055edf8260403d814827ced1595004
Whatfck <daniel@LAB.tailbb818c.ts.net>
2025-11-30 13:54:26 +0000

    update 3

---
+++ config/media/jellyfin/data/ScheduledTasks/3a025083-141d-3c17-dd96-d5f9b951287b.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2025-11-30T13:37:42.7173416Z","EndTimeUtc":"2025-11-30T13:37:42.7357413Z","Status":"Completed","Name":"Clean up collections and playlists","Key":"CleanCollectionsAndPlaylists","Id":"3a025083141d3c17dd96d5f9b951287b"}
---
+++ config/media/jellyfin/data/ScheduledTasks/7d8088c1-0902-f1bf-4072-ded42437bcfb.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2025-11-30T13:37:42.7174987Z","EndTimeUtc":"2025-11-30T13:37:42.7229498Z","Status":"Completed","Name":"Clean Transcode Directory","Key":"DeleteTranscodeFiles","Id":"7d8088c10902f1bf4072ded42437bcfb"}
